9f4f7c8e59e52f8545d5ec8538e62aa1e74be168
vpro <wwwtech@vpro.nl>
2012-07-18 15:10:55 +0000

    [maven-release-plugin]  copy for tag REL-1.1.1-SNAPSHOT

---
+++ REL-1.1-SNAPSHOT/src/main/java/nl/vpro/esper/service/Statement.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2012 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.esper.service;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.UpdateListener;
+
+public class Statement {
+    private String epl;
+
+    private EPStatement epStatement;
+
+    private Set<UpdateListener> listeners = new LinkedHashSet<UpdateListener>();
+
+    public Statement(String epl) {
+        this.epl = epl;
+    }
+
+    public String getEPL() {
+        return epl;
+    }
+
+    public void setListeners(UpdateListener... listeners) {
+        for(UpdateListener listener : listeners) {
+            addListener(listener);
+        }
+    }
+
+    public void addListener(UpdateListener listener) {
+        listeners.add(listener);
+        if(epStatement != null) {
+            epStatement.addListener(listener);
+        }
+    }
+
+    void setEPStatement(EPStatement epStatement) {
+        this.epStatement = epStatement;
+        for(UpdateListener listener : listeners) {
+            epStatement.addListener(listener);
+        }
+    }
+}

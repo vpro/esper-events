06c5571dae8c35d688246d98b7fbbc802578abae
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-06-13 23:36:27 +0200

    access to builders.

--- src/main/java/nl/vpro/esper/service/EventServiceProvider.java
+++ src/main/java/nl/vpro/esper/service/EventServiceProvider.java
@@ -10,4 +10,12 @@ public interface EventServiceProvider {
 
     void send(Object event);
 
+    static EventServiceProviderImpl.Builder builder() {
+        return EventServiceProviderImpl.builder();
+    }
+
+    static AsyncEventServiceProviderImpl.Builder async() {
+        return AsyncEventServiceProviderImpl.asyncBuilder();
+    }
+
 }
